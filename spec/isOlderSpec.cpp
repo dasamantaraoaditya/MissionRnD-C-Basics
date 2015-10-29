85e4186035e6b03b3797153bea674896496e24ce
hemanth.chinthalapudi <hemanth.chinthalapudi@kony.com>
2015-10-29 20:19:13 +0530

    Corrected isOlder test cases.

--- spec/isOlderSpec.cpp
+++ spec/isOlderSpec.cpp
@@ -27,12 +27,12 @@ namespace spec
 
 		TEST_METHOD(elder)
 		{
-			Assert::AreEqual(2, isOlder("15-07-1992", "15-07-2000"), L"isOlder: Person two is elder", LINE_INFO());
-			Assert::AreEqual(1, isOlder("29-02-2004", "29-02-2000"), L"isOlder: Person one is elder", LINE_INFO());
-			Assert::AreEqual(2, isOlder("15-05-2000", "15-07-2000"), L"isOlder: Person two is elder", LINE_INFO());
-			Assert::AreEqual(1, isOlder("29-06-2000", "29-02-2000"), L"isOlder: Person one is elder", LINE_INFO());
-			Assert::AreEqual(2, isOlder("11-07-2000", "15-07-2000"), L"isOlder: Person two is elder", LINE_INFO());
-			Assert::AreEqual(1, isOlder("29-02-2000", "27-02-2000"), L"isOlder: Person one is elder", LINE_INFO());
+			Assert::AreEqual(1, isOlder("15-07-1992", "15-07-2000"), L"isOlder: Person one is elder", LINE_INFO());
+			Assert::AreEqual(2, isOlder("29-02-2004", "29-02-2000"), L"isOlder: Person two is elder", LINE_INFO());
+			Assert::AreEqual(1, isOlder("15-05-2000", "15-07-2000"), L"isOlder: Person one is elder", LINE_INFO());
+			Assert::AreEqual(2, isOlder("29-06-2000", "29-02-2000"), L"isOlder: Person two is elder", LINE_INFO());
+			Assert::AreEqual(1, isOlder("11-07-2000", "15-07-2000"), L"isOlder: Person one is elder", LINE_INFO());
+			Assert::AreEqual(2, isOlder("29-02-2000", "27-02-2000"), L"isOlder: Person two is elder", LINE_INFO());
 		}
 	};
 }
